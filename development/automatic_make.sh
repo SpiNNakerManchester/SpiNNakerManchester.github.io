51c880b06eb16f49a28320b7d385c2ae19b3f194
Andrew Gait <andrew.gait@manchester.ac.uk>
2017-08-04 11:21:00 +0100

    Update development environment instructions
    - generally, remove unnecessary repositories
    - add new(er) repositories, including spalloc
    - add images for va_benchmark in pynn7 and pynn8
    - update automatic_make script
    - does anything else need editing in here?

--- development/automatic_make.sh
+++ development/automatic_make.sh
@@ -1,4 +1,8 @@
+# This script assumes it is run from the directory holding all github projects in parellel
+# sh SupportScripts/automatic_make.sh
+
 cd spinnaker_tools
+source $PWD/setup
 make clean
 make || exit $?
 cd ..
@@ -7,9 +11,10 @@ make clean
 make || exit $?
 make install
 cd ..
-cd SpiNNMan/c_models/
+cd SpiNNMan/c_models/reinjector/
+make clean
 make || exit $?
-cd ../..
+cd ../../..
 cd SpiNNFrontEndCommon/c_common/
 cd front_end_common_lib/
 make install-clean
@@ -18,17 +23,11 @@ make clean
 make || exit $?
 make install
 cd ../..
-cd SpiNNakerGraphFrontEnd/spinnaker_graph_front_end/examples/
+cd sPyNNaker/neural_modelling/
 make clean
 make || exit $?
-cd ../../../sPyNNaker/neural_modelling/
-make clean
-make || exit $?
-source setup
-cd ../../sPyNNakerExternalDevicesPlugin/neural_modelling/
-make clean
-make || exit $?
-cd ../../sPyNNakerExtraModelsPlugin/neural_modelling
+source $PWD/setup
+cd ../../SpiNNakerGraphFrontEnd/spinnaker_graph_front_end/examples/
 make clean
 make || exit $?
 echo "completed"
